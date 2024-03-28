a11bf9fce15cb072e6c2280a70afa83f0e2764f4
David Marom <davidmarom.pro@gmail.com>
2024-03-28 18:49:30 +0200

    wip

--- test.js
+++ test.js
@@ -1,12 +1,47 @@
+// Write a promise that resolves after 1000 with value 1000
+// Same for 2000 and 3000
+
 aaa = new Promise(
-    (res, rej) => {
-        let a = 5;
-        if (a === 3) res('3');
-        else rej(5);
+    (resolve, reject) => {
+        setTimeout(() => { resolve(1000); }, 2000)
     }
-
-
+)
+bbb = new Promise(
+    (resolve, reject) => {
+        setTimeout(() => { resolve(2000); }, 1000)
+    }
 )
 
-aaa.then((x) => console.log(x))
-    .catch((x) => console.log(x, 'bad'))
+// myRA gets: array of promises
+// returns array of answers from each fromise
+// actually returns a promise that resolves an array of answers...
+
+const myRA = (arr) => {
+    let count = 0;
+    let retArr = [];
+
+    return new Promise((ress) => {
+
+        arr.forEach((element, idx) => {
+            element.then(
+                (res) => {
+                    retArr[idx] = res;
+                    count++;
+                    console.log('debug', retArr, count, arr.length)
+                    if (count === 2) { ress(retArr) }
+                }
+            )
+        }
+        );
+
+    })
+
+
+}
+// if (count === arr.length) { return new Promise((resolve) => { resolve(retArr) }) }
+
+myRA([aaa, bbb]).then(
+    (res) => {
+        console.log(res)
+    }
+)
